8f9a6d5537e18caa02a5d61897f89404adb81dce
george ghobrial <georgeheshmet@gmail.com>
2020-07-18 00:41:40 +0200

    removing  console logs used for debugging

--- website/app.js
+++ website/app.js
@@ -28,8 +28,6 @@ const post_data =async function postData(url,data){
       });
     try{
 
-        const data= res.json();
-        console.log(data);
     } catch(error){
         console.log("error",error);
     }
@@ -47,13 +45,10 @@ function update_weather_info()
     const zipcode=document.getElementById("zip");
     const user_feeling=document.getElementById("feelings");
     /*debugging for checked conditions*/
-    console.log(Number.isInteger(parseInt((zipcode.value),10)))
-    console.log(zipcode.value.length==5);
     /*if zip code format and length is ok get weather data, post data to server and update UI*/
     if(Number.isInteger(parseInt((zipcode.value),10)) && (zipcode.value.length==5)){
         get_data(fixed_weath_url+zipcode.value+"&appid="+weather_api_key).then(function(data){
         post_data("/addWeathData",{Current_temp:data.main.temp, date:newDate, UserFeeling:user_feeling.value}).then(updateUI);
-        //post_data("/addWeathData","test")
         });
     }
     else{
@@ -69,7 +64,6 @@ const updateUI =async function update_UI(){
     /*getting last entry from server*/
     const res=await get_data("/GetLastEntry");
     try{
-        console.log("data from server 2"+res);
         temp.textContent= `Current temperature: ${res.Current_temp}`;
         date.textContent= `Date: ${res.date}`;
         content.textContent= `User input: ${res.UserFeeling}`;
